d0c17ec9eb2b4413b17b8790549222457fcb0c0b
Abzal Mautaliev <abzalmautaliev@outlook.com>
2022-02-16 01:21:01 +0600

    ноу хау, шутка, нет

--- Program.cs
+++ Program.cs
@@ -4,56 +4,153 @@ namespace lab15
 {
     class Program
     {
+        static string[] resultActionList = new string[] { };
         static void Main(string[] args)
         {
             Console.WriteLine(" # | Действие");
             Console.WriteLine("---|---------------------------------");
-            string buffer = variants[0];
+            Console.WriteLine(" 1 | Начало дня");
 
-            int plan = Randomizer(2);
-            string list = string.Empty;
-            switch(plan)
+            int action1 = ChooseVariant(1, 2);
+
+            switch (action1)
             {
-            case 1:
-                list = variants[plan];
-                insert(ref buffer, list);
-            break;
-            case 2:
-                list = variants[plan];
-                insert(ref buffer, list);
-            break;
-            default:
-                Console.WriteLine();
-            break;
+                case 1:
+                    ChooseAction(1);
+                    break;
+                case 2:
+                    ChooseAction(2);
+                    break;
+                default:
+                    break;
+            }
+            if(action1 == 1)
+            {
+                int action2 = ChooseVariant(1, 10);
+                switch(action2)
+                {
+                    case 1: case 2:case 3:case 4:case 5:case 6:case 7:
+                        ChooseAction(3);
+                        break;
+                    case 8:case 9:case 10:
+                        int action3 = ChooseVariant(1, 6);
+                        switch(action3)
+                        {
+                        case 1:case 2:case 3:case 4:
+                            ChooseAction(4);
+                            break;
+                        case 5:case 6:
+                            ChooseAction(5);
+                            break;
+                        default:
+                        break;
+                        }
+                    break;
+                    default:
+                    break;
+                }
+            }
+            else
+            {
+                int action3 = ChooseVariant(1, 6);
+                switch(action3)
+                {
+                    case 1:case 2:case 3:case 4:
+                        ChooseAction(4);
+                        break;
+                    case 5:case 6:
+                        ChooseAction(5);
+                        break;
+                    default:
+                    break;
+                }
+            }
+            int action4 = ChooseVariant(1, 12);
+            switch(action4)
+            {
+                case 1: case 2:case 3:case 4:case 5:case 6:case 7:case 8:
+                    ChooseAction(6);
+                    break;
+                case 9:case 10:case 11:case 12:
+                    int action5 = ChooseVariant(1, 4);
+                    switch(action5)
+                    {
+                        case 1:
+                            ChooseAction(7);
+                            break;
+                        case 2:case 3:case 4:
+                            ChooseAction(8);
+                            break;
+                        default:
+                        break;
+                    }
+                break;
+                default:
+                break;
             }
 
-            plan = Randomizer(10);
-
-            PrintResultActionList(buffer);
-        }
-
-        static int Randomizer(int a)
-        {
-            Random randomLevel = new Random();
-            int initialLevel = randomLevel.Next(1, a + 1);
-            //Console.WriteLine(initialLevel);
-            return initialLevel;
-        }
-
-        static string insert(ref string resultActionString, string value)
-        {
-            resultActionString = resultActionString + "; " + value;
-            return resultActionString;
-        }
-        static string[] PrintResultActionList(string resultActionString)
-        {
-            string[] resultActionList = resultActionString.Split("; ");
-            for(int i = 0; i < resultActionList.Length; i++)
+            /*switch (action2)
             {
-                Console.WriteLine($" {i + 1} | {resultActionList[i]}");
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    ChooseAction(3);
+                    break;
+                case 8: 
+                case 9: 
+                case 10:
+                    int action3 = ChooseVariant(1, 6);
+                    switch (action3)
+                    {
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:
+                            ChooseAction(4);
+                            break;
+                        case 5:
+                        case 6:
+                            ChooseAction(5);
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
             }
+            */
+            PrintResultActionList(resultActionList);
+        }
+
+        static string[] ChooseAction(int b)
+        {
+            string a = variants[b];
+            Array.Resize(ref resultActionList, resultActionList.Length + 1);
+            resultActionList[resultActionList.Length - 1] = a;
             return resultActionList;
         }
+
+        static string[] PrintResultActionList(string[] a)
+        {
+            for(int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine($" {i + 2} | {resultActionList[i]}");
+            }
+            return a;
+        }
+
+        static int ChooseVariant(int a, int b)
+        {
+            Random randomLevel = new Random();
+            int initialLevel = randomLevel.Next(a, b + 1);
+            return initialLevel;
+        }
+        
 
         static string[] variants = {
             "Начало дня",
