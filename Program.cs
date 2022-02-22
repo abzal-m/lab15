dab684c8b2afd119a536eeae6fb9784fe983a064
Abzal Mautaliev <abzalmautaliev@outlook.com>
2022-02-23 02:42:08 +0600

    Решение

--- Program.cs
+++ Program.cs
@@ -5,176 +5,156 @@ namespace lab15
     class Program
     {
         static string[] resultActionList = new string[] { };
+        static Random randomLevel = new Random();
         static void Main(string[] args)
         {
-            Console.WriteLine(" # | Действие");
-            Console.WriteLine("---|---------------------------------");
-            Console.WriteLine(" 1 | Начало дня");
-
-            int action1 = ChooseVariant(1, 2);
-            switch (action1)
-            {
-                case 1:
-                    ChooseAction(1);
-                    break;
-                case 2:
-                    ChooseAction(2);
-                    break;
-                default:
-                    break;
-            }
-            if(action1 == 1)
-            {
-                int action2 = ChooseVariant(1, 10);
-                switch(action2)
-                {
-                    case 1: case 2:case 3:case 4:case 5:case 6:case 7:
-                        ChooseAction(3);
-                        break;
-                    case 8:case 9:case 10:
-                        int action3 = ChooseVariant(1, 6);
-                        switch(action3)
-                        {
-                        case 1:case 2:case 3:case 4:
-                            ChooseAction(4);
-                            break;
-                        case 5:case 6:
-                            ChooseAction(5);
-                            break;
-                        default:
-                        break;
-                        }
-                    break;
-                    default:
-                    break;
-                }
-            }
-            else
-            {
-                int action3 = ChooseVariant(1, 6);
-                switch(action3)
-                {
-                    case 1:case 2:case 3:case 4:
-                        ChooseAction(4);
-                        break;
-                    case 5:case 6:
-                        ChooseAction(5);
-                        break;
-                    default:
-                    break;
-                }
-            }
-
-            int action4 = ChooseVariant(1, 12);
-            switch(action4)
-            {
-                case 1: case 2:case 3:case 4:case 5:case 6:case 7:case 8:
-                    ChooseAction(6);
-                    break;
-                case 9:case 10:case 11:case 12:
-                    int action5 = ChooseVariant(1, 4);
-                    switch(action5)
-                    {
-                        case 1:
-                            ChooseAction(7);
-                            break;
-                        case 2:case 3:case 4:
-                            ChooseAction(8);
-                            break;
-                        default:
-                        break;
-                    }
-                break;
-                default:
-                break;
-            }
-            int action6 = ChooseVariant(1, 5);
-            switch(action6)
-            {
-                case 1: case 2:
-                    ChooseAction(9);
-                    int action7 = ChooseVariant(1, 8);
-                    switch(action7)
-                        {
-                            case 1: case 2:
-                                ChooseAction(10);
-                                int action8 = ChooseVariant(1, 8);
-                                switch(action8)
-                                {
-                                    case 1: case 2:
-                                        ChooseAction(13);
-                                        break;
-                                    case 3:case 4:case 5:case 6:case 7:case 8:
-                                        ChooseAction(14);
-                                        break;
-                                    default:
-                                        break;
-                                }
-                                break;
-                            case 3:case 4:case 5:case 6:case 7:case 8:
-                                ChooseAction(11);
-                                int action9 = ChooseVariant(1, 2);
-                                switch (action9)
-                                {
-                                    case 1:
-                                        ChooseAction(13);
-                                        break;
-                                    case 2:
-                                        ChooseAction(14);
-                                        break;
-                                    default:
-                                        break;
-                                }
-                                break;
-                            default:
-                                break;
-                        }
-                        break;
-                case 3:case 4:case 5:
-                    ChooseAction(12);
-                    int action10 = ChooseVariant(1, 8);
-                    switch(action10)
-                    {
-                        case 1: case 2:case 3:case 4:case 5:
-                            ChooseAction(13);
-                            break;
-                        case 6:case 7:case 8:
-                            ChooseAction(14);
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-
-
-            }
-
-           
+            ChooseAction(0);
             PrintResultActionList(resultActionList);
         }
 
-        static string[] ChooseAction(int b)
+        static void ChooseAction(int b)
         {
             string a = variants[b];
             Array.Resize(ref resultActionList, resultActionList.Length + 1);
             resultActionList[resultActionList.Length - 1] = a;
-            return resultActionList;
-        }
 
-        static string[] PrintResultActionList(string[] a)
-        {
-            for(int i = 0; i < a.Length; i++)
+            switch(a)
             {
-                Console.WriteLine($" {i + 2} | {resultActionList[i]}");
+            case "Начало дня":
+                int random = ChooseVariant(1, 2);
+                if(random == 1)
+                {
+                    ChooseAction(1);
+                }
+                else
+                {
+                    ChooseAction(2);
+                }
+                break;
+            case "Поднимаюсь с кровати с левой ноги":
+                random = ChooseVariant(7, 10);
+                if(random == 1)
+                {
+                    ChooseAction(3);
+                }
+                else
+                {
+                    goto case "Поднимаюсь с кровати с правой ноги";
+                }
+                break;
+            case "Поднимаюсь с кровати с правой ноги":
+                random = ChooseVariant(4, 6);
+                if(random == 1)
+                {
+                    ChooseAction(4);
+                }
+                else
+                {
+                    ChooseAction(5);
+                }
+                break;
+            case "Падаю с кровати" or "Пробежка по аллее" or "Физ. зарядка на балконе":
+                random = ChooseVariant(8, 12);
+                if(random == 1)
+                {
+                    ChooseAction(6);
+                }
+                else
+                {
+                    goto case "Готовлю плотный завтрак";
+                }
+                break;
+            case "Готовлю плотный завтрак":
+                random = ChooseVariant(1, 4);
+                if(random == 1)
+                {
+                    ChooseAction(7);
+                }
+                else
+                {
+                    ChooseAction(8);
+                }
+                break;
+            case "Варю крепкий кофе" or "Готовлю плотный завтрак" or "Достаю вчерашний салат":
+                random = ChooseVariant(2, 5);
+                if(random == 1)
+                {
+                    ChooseAction(9);
+                }
+                else
+                {
+                    ChooseAction(12);
+                }
+                break;
+            case "Спустило колесо":
+                random = ChooseVariant(2, 8);
+                if(random == 1)
+                {
+                    ChooseAction(10);
+                }
+                else
+                {
+                    ChooseAction(11);
+                }
+                break;
+            case "Добираюсь на работу пешком":
+                random = ChooseVariant(2, 8);
+                if(random == 1)
+                {
+                    ChooseAction(13);
+                }
+                else
+                {
+                    ChooseAction(14);
+                }
+                break;
+            case "Добираюсь на работу на маршрутке":
+                random = ChooseVariant(1, 2);
+                if(random == 1)
+                {
+                    ChooseAction(13);
+                }
+                else
+                {
+                    ChooseAction(14);
+                }
+                break;
+            case "Добираюсь на работу на своем авто":
+                random = ChooseVariant(5, 8);
+                if(random == 1)
+                {
+                    ChooseAction(13);
+                }
+                else
+                {
+                    ChooseAction(14);
+                }
+                break;
+            case "Прихожу вовремя" or "Опаздываю на работу":
+                ChooseAction(15);
+                break;
             }
-            return a;
         }
-
-        static int ChooseVariant(int a, int b)
+        static void PrintResultActionList(string[] ActionList)
         {
-            Random randomLevel = new Random();
-            int initialLevel = randomLevel.Next(a, b + 1);
-            return initialLevel;
+            Console.WriteLine(" # | Действие");
+            for(int i = 0; i < ActionList.Length; i++)
+            {
+                Console.WriteLine($" {i + 1} | {resultActionList[i]}");
+            }
+        }
+        static void HappyCounter(int day)
+        { 
+            day = day - 1;
+        }
+        static int ChooseVariant(int maxForFirstVariant, int maxForSecondVariant)
+        {
+            int initialLevel = randomLevel.Next(1, maxForSecondVariant + 1);
+            if (initialLevel <= maxForFirstVariant)
+            {
+                return 1;
+            }
+            return 2;
         }
         
 
